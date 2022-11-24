fd8277d6976b6d725c56833cf17446d3b8064856
taras <ghoostpost@gmail.com>
2022-11-24 21:16:09 +0200

    memory_game_0.2

--- MemoryGame.py
+++ MemoryGame.py
@@ -1,8 +1,18 @@
 import random
 import time
 
-rand_list=[]
-generate_sequence = int(input("Choose a difficulty of Game: "))
-for i in range(generate_sequence):
-    rand_list.append(random.randint(1, 101))
-print(rand_list)
+
+def printw(string):
+    print(string)
+    time.sleep(0.7)
+
+
+generate_number_of_difficulty = int(input("Choose numbers of difficulty from 1 to 101: "))
+
+
+def generate():
+    list_diff = []
+    generate_sequence = int(input(f"How many numbers you want to remember from {generate_number_of_difficulty}: "))
+    for i in range(generate_sequence):
+        list_diff.append(random.randint(1, generate_number_of_difficulty))
+    print(list_diff)
