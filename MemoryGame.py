afa1a67aa9a950641070e590dd08bee2b3577c4d
taras <ghoostpost@gmail.com>
2022-12-01 18:22:52 +0200

    memory_game_realise1

--- MemoryGame.py
+++ MemoryGame.py
@@ -2,27 +2,28 @@ import random
 import time
 
 
+def game():
+    generate_number_of_difficulty = int(input("Choose numbers of difficulty from 1 to 101: "))
+    generate_sequence = int(input(f"How many numbers you want to remember to {generate_number_of_difficulty}: "))
+    rand_list = []
+
+    for i in range(0, generate_sequence):
+        rand_list.append(random.randint(1, generate_sequence))
+    print(rand_list)
+
+    time.sleep(0.7)
+    for i in range(0, 100):
+        print('')
+
+    for i in range(0, generate_sequence):
+        print("Enter number you remember")
+        guess = int(input())
+        if guess == rand_list[i]:
+            print("Good")
+        else:
+            print("Bad")
+            break
 
 
-generate_number_of_difficulty = int(input("Choose numbers of difficulty from 1 to 101: "))
-generate_sequence = int(input(f"How many numbers you want to remember to {generate_number_of_difficulty}: "))
 
-rand_list=[]
-
-for i in range(0, generate_sequence):
-    rand_list.append(random.randint(1, generate_sequence))
-print(rand_list)
-
-time.sleep(0.7)
-for i in range(0, 100):
-    print('')
-
-for i in range(0, generate_sequence):
-    print("Enter number you remember")
-    guess = int(input())
-    if guess == rand_list[i]:
-        print ("good")
-    else:
-        print("Bad")
-        break
-
+game()
