c36ea5a963055015f3e191ac0141ec7f5ad4190d
taras <ghoostpost@gmail.com>
2022-12-06 23:49:51 +0200

    Currency_realise_0.1

--- CurrencyRouletteGame.py
+++ CurrencyRouletteGame.py
@@ -1,46 +1,27 @@
-from forex_python.converter import CurrencyRates
+from currency_converter import CurrencyConverter
 import random
-from datetime import date
+
+
+
 
 
 def get_money_interval():
-    difficult = int(input('Choose difficult: '))
-    t = CurrencyRates()
-
+    difficult = int(input('Choose difficult from 1 to 100: '))
+    t = CurrencyConverter()
     usd = int(random.randint(1, 100))
-    t.convert(usd, 'ILS')
-    print(t)
+    print(usd, "$")
+    con = t.convert(usd, 'USD', 'ILS')
+    print(con, "₪")
+    high = int(con - (5 - difficult))
+    low = int(con + (5 - difficult))
+    print(low, high)
 
-    result = usd * t
-    low = int(result - (5 - difficult))
-    high = int(result + (5 - difficult))
-    print(t)
-    #
-    return usd, t, low, high
+    guess = int(input(f"Guess the value of {usd}$ in ILS: "))
 
+    if low <= guess <= high:
+        print("True")
+    else:
+        print("False")
 
-#
-#
-# def get_guess_from_user(usd):
-#     # User needs to guess the Value to a given amount of USD
-#     while True:
-#         try:
-#             guess = int(input(f"Guess the value of {usd}$ in ILS: "))
-#         except ValueError:
-#             print("Error: Enter just numbers please, not letters, words ,etc...")
-#             continue
-#         return guess
-#
-
-#
-# # def play(difficulty):
-# #     # Will call all other functions to start the game
-# #     usd, t, low, high = get_money_interval(difficulty)
-# #     guess = get_guess_from_user(usd)
-# #     os.system('cls' if os.name == 'nt' else 'clear')
-# #     if high >= guess >= low:
-# #         return True
-# #     else:
-# #         return False
 
 get_money_interval()
